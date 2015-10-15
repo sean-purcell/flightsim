43f524f954a613a4e0da91f295ebafbb8b2576aa
Sean Purcell <iburinoc@gmail.com>
2015-10-15 13:13:09 -0400

    basic plane physics

--- flightsim/aircraft.hpp
+++ flightsim/aircraft.hpp
@@ -5,22 +5,24 @@ class Aircraft: public Drawable	//currently a hacked together proof of concept u
 	//IT DOESN'T INHERIT FROM SPHERE BECAUSE IT WILL EVENTUALLY NOT BE A SPHERE (hopefully)
 {
 	public:
-		float mass;
-		float thrust;
-		float aoi;
+		float g = -9.8;
+		float mass = 23500;
+		float thrust = 62300 * 2;
+		float aoi = 10 * M_PI / 180;
 		float maxaileron;
-		float wingarea;
+		float wingarea = 38;
 		float aileronarea;
 		float aileronradius;
 		float elevatorarea;
 		float elevatorradius;
+		float rho = 1.225;
 
 		Quaternion pos, facing, velocity;
 		
 		Aircraft(){
-			pos = Quaternion(0, 1, 1, 1);		//real component must be zero
+			pos = Quaternion(0, 1, 100, 1);		//real component must be zero
 			facing = Quaternion (1, 0, 0, 0);	//orientation
-			velocity = Quaternion (0, 0, 0, 0);	//real component must be zero
+			velocity = Quaternion (0, 0, 0, 531);	//real component must be zero
 			
 			//bunch of sphere stuff that will be obsolete eventually
 			radius = 0.05;
@@ -54,42 +56,50 @@ class Aircraft: public Drawable	//currently a hacked together proof of concept u
 		}
 		
 		void update(float dt){
-			pos = pos + velocity*dt;
+			applyForces(dt);
 		}
 		
 		void draw(sf::RenderWindow &window){
 			window.draw(shape);
 		}
 
-		void computeForces() {
+		void applyForces(float dt) {
 			Quaternion netF(0, 0, 0, 0);
 			Quaternion netT(0, 0, 0, 0);
 
-#ifdef UNDEFINED
-			netF += fGravity();
-			netF += fWing();
-			netF += fAileron();
-			netF += fElevator();
-			netF += fThrust();
+			netF = netF + fGravity();
+			netF = netF + fWing();
+			//netF += fAileron();
+			//netF += fElevator();
+			netF = netF + fThrust();
 
-			netT += tAileron();
-			netT += tElevator();
-#endif
+			//netT += tAileron();
+			//netT += tElevator();
+
+			Quaternion accel = netF * (1 / mass);
+
+			this->pos = this->pos + this->velocity * dt + accel * (0.5f * dt * dt);
+			this->velocity = this->velocity + accel * dt;
+
+			std::cout << "s:" << this->pos << ", v:" << this->velocity << ", a:" << accel << ", f:" << (facing.transform(Quaternion(0, 0, 1))) << std::endl;
 		}
 
 		Quaternion fGravity() {
-			return Quaternion(0, 0, 9.8 * mass, 0);
+			return Quaternion(0, 0, g * mass, 0);
 		}
 
 		Quaternion fWing() {
-			Quaternion wingang(aoi, Quaternion(0, 0, 1));
-			Quaternion wp = facing.transform(wingang.transform(Quaternion(1, 0, 0)));
+			Quaternion wingang(aoi, Quaternion(-1, 0, 0));
 			Quaternion wn = facing.transform(wingang.transform(Quaternion(0, 1, 0)));
-			return Quaternion(0, 0, 0, 0);
+			Quaternion v = this->velocity * -1.0f;
+
+			Quaternion lift = wn * rho * wingarea * fabs(v.dot(wn)) * (v.dot(wn));
+			std::cout << "wn: " << wn << ", lift: " << lift << std::endl;
+			return lift;
 		}
 
 		Quaternion fThrust() {
-			Quaternion fw = facing.transform(Quaternion(1, 0, 0));
+			Quaternion fw = facing.transform(Quaternion(0, 0, 1));
 			return fw * thrust;
 		}
 		
