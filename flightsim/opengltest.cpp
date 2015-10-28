027b7ef8b17e7b6b58dfa0b6639db48ee7b864b7
Sean Purcell <iburinoc@gmail.com>
2015-10-27 20:00:48 -0400

    got something working

--- flightsim/opengltest.cpp
+++ flightsim/opengltest.cpp
@@ -1,76 +1,81 @@
-#define GLEW_STATIC
+// The OpenGL libraries, make sure to include the GLUT and OpenGL frameworks
+#define  GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#include <OpenGL/gl3.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
 
-#include <SFML/Window.hpp>
-#include <iostream>
-#include <vector>
 #include <ctime>
 #include <chrono>
 #include <cmath>
+#include <iostream>
+#include <vector>
 
-#include "openglheaders.hpp"
-
-#include "shaders.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// This is just an example using basic glut functionality.
+// If you want specific Apple functionality, look up AGL
+
 // Shader sources
 const GLchar* vertexSource =
-    "#version 120\n"
-    "vec2 position;"
+    "#version 150 core\n"
+    "in vec2 position;"
+    "in vec3 color;"
+    "out vec3 Color;"
+    "uniform mat4 trans;"
     "void main() {"
-    "   gl_Position = vec4(position, 0, 1);"
+    "   Color = color;"
+    "   gl_Position = trans * vec4(position, 0.0, 1.0);"
     "}";
 const GLchar* fragmentSource =
-    "#version 120\n"
-    "vec4 outColor;"
+    "#version 150 core\n"
+    "in vec3 Color;"
+    "out vec4 outColor;"
     "void main() {"
-    "   outColor = vec4(1.0f,1.0f,1.0f,1.0f);"
+    "   outColor = vec4(Color, 1.0);"
     "}";
 
-int fov=90;
-float ratio=std::tan(fov/2.0);
-int screenwidth=800;
-int screenheight=600;
-sf::Vector2i size(screenwidth, screenheight);
-
-int main() {
-	auto t_start = std::chrono::high_resolution_clock::now();
-	sf::ContextSettings settings;
-	settings.depthBits = 24;
-	settings.stencilBits = 8;
-
-	sf::Window window(sf::VideoMode(800, 600, 32), "OpenGL",
-		sf::Style::Titlebar | sf::Style::Close, settings);
-
-#ifndef __APPLE__
-	glewExperimental = GL_TRUE;
-	glewInit();
-#endif
-
-	std::cout << vertexSource << std::endl;
-// Create Vertex Array Object
+std::chrono::high_resolution_clock::time_point t_start;
+GLint uniModel;
 	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	// Create a Vertex Buffer Object and copy the vertex data to it
 	GLuint vbo;
-	glGenBuffers(1, &vbo);
 
-	GLfloat vertices[] = {
-		0.0f, -1.0f,
-		0.0f, 1.0f,
-		1.0f, 0.0f,
-	};
+void init() // Called before main loop to set up the program
+{
+// Create Vertex Array Object
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    glGenBuffers(1, &vbo);
 
-	// Create and compile the vertex shader
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
-	glCompileShader(vertexShader);
+    GLfloat vertices[] = {
+        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create an element array
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
+    GLuint elements[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
 
 	GLint success = 0;
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -83,11 +88,10 @@ int main() {
 		std::cout << &errorLog[0] << std::endl;
 	}
 
-	// Create and compile the fragment shader
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-	glCompileShader(fragmentShader);
-
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
 	success = 0;
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if(success != GL_TRUE) {
@@ -99,53 +103,88 @@ int main() {
 		std::cout << &errorLog[0] << std::endl;
 	}
 
-	// Link the vertex and fragment shader into a shader program
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glBindFragDataLocation(shaderProgram, 0, "outColor");
-	glLinkProgram(shaderProgram);
-	glUseProgram(shaderProgram);
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
 
-	// Specify the layout of the vertex data
-	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
 
-	while (window.isOpen())
-	{
-		sf::Event windowEvent;
-		while (window.pollEvent(windowEvent))
-		{
-			switch (windowEvent.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			}
-		}
+    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
 
-		// Clear the screen to black
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+    GLint uniTrans = glGetUniformLocation(shaderProgram, "trans");
+    glm::mat4 trans(1);
+    trans[1][0] = 0.7;
+    glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(trans));
 
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(0.0f, 1.0f);
-		glVertex2f(1.0f, 0.0f);
-		glVertex2f(0.0f, -1.0f);
-		glEnd();
-
-		// Swap buffers
-		window.display();
-	}
-
-	glDeleteProgram(shaderProgram);
-	glDeleteShader(fragmentShader);
-	glDeleteShader(vertexShader);
-
-	glDeleteBuffers(1, &vbo);
-
-	glDeleteVertexArrays(1, &vao);
+std::cout << "init done\n";
 }
 
+// Called at the start of the program, after a glutPostRedisplay() and during idle
+// to display a frame
+void display()
+{
+
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+ // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a rectangle from the 2 triangles using 6 indices
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glutSwapBuffers();
+std::cout << "render\n";
+}
+
+// Called every time a window is resized to resize the projection matrix
+void reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-0.1, 0.1, -float(h)/(10.0*float(w)), float(h)/(10.0*float(w)), 0.5, 1000.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv); // Initializes glut
+
+    // Sets up a double buffer with RGBA components and a depth component
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA | GLUT_3_2_CORE_PROFILE);
+
+    // Sets the window size to 512*512 square pixels
+    glutInitWindowSize(512, 512);
+
+    // Sets the window position to the upper left
+    glutInitWindowPosition(0, 0);
+
+    // Creates a window using internal glut functionality
+    glutCreateWindow("Hello!");
+
+    // passes reshape and display functions to the OpenGL machine for callback
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+std::printf("%s\n%s\n", 
+        glGetString(GL_RENDERER),  // e.g. Intel HD Graphics 3000 OpenGL Engine
+        glGetString(GL_VERSION)    // e.g. 3.2 INTEL-8.0.61
+        );
+
+    init();
+
+    // Starts the program.
+    glutMainLoop();
+    return 0;
+}
