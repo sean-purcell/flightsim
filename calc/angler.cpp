08ab2383284ccc15f21d30cc2d7b10a7d32b99a6
ahiijny <ahiijny@gmail.com>
2015-11-05 03:30:48 -0500

    Refactor angler.

--- calc/angler.cpp
+++ calc/angler.cpp
@@ -1,5 +1,19 @@
 #include "angler.hpp"
 #include <cassert>
+#include <cstdlib>
+#include <algorithm>
+
+
+ double STICK[] = {0, 1, 0};        // direction of gravity when controller is in rest state
+ double PITCH_AXIS[] = {1, 0, 0};   // direction of pitch axis when controller is in rest state
+ double ROLL_AXIS[] = {0, 0, -1};   // direction of roll axis when controller is in rest state
+
+ double PITCH_PERP[] = {0, 0, 1};   // vector rotated 90 degrees counterclockwise about PITCH_AXIS from STICK direction
+ double ROLL_PERP[] = {1, 0, 0};    // vector rotated 90 degrees counterclockwise about ROLL_AXIS from STICK direction
+
+ double scale_pitch = 1 / 3.0;      // responsiveness of controller [rad/s per rad]
+ double scale_roll = 4 / 3.0;       // responsiveness of controller [rad/s per rad]
+ double max_theta = acos(-1) / 2;   // magnitude of max rotation of controller; input clipped at this value if input is higher
 
 /** Check that the rotation axes make sense.
  */
@@ -8,10 +22,10 @@ void check_asserts()
     assert(sqmag(STICK) == 1);
     assert(sqmag(PITCH_AXIS) == 1);
     assert(sqmag(ROLL_AXIS) == 1);
-    
+
     assert(sqmag(PITCH_PERP) == 1);
     assert(sqmag(ROLL_PERP) == 1);
-    
+
     assert(dot(STICK, PITCH_AXIS) == 0);
     assert(dot(PITCH_AXIS, ROLL_AXIS) == 0);
     assert(dot(STICK, ROLL_AXIS) == 0);
@@ -23,7 +37,7 @@ void check_asserts()
  */
 double pitcher(int gx, int gy, int gz)
 {
-    double g[] = {gx, gy, gz};
+    double g[] = {1.0*gx, 1.0*gy, 1.0*gz};
     double g_stick = dot(g, STICK);
     double g_perp = dot(g, PITCH_PERP);
     double theta = -atan2(g_perp, g_stick); // the angle of g wrt controller is the negative of the angle of the controller wrt g
@@ -36,7 +50,7 @@ double pitcher(int gx, int gy, int gz)
  */
 double roller(int gx, int gy, int gz)
 {
-    double g[] = {gx, gy, gz};
+    double g[] = {1.0*gx, 1.0*gy, 1.0*gz};
     double g_stick = dot(g, STICK);
     double g_perp = dot(g, ROLL_PERP);
     double theta = -atan2(g_perp, g_stick); // the angle of g wrt controller is the negative of the angle of the controller wrt g
@@ -75,12 +89,6 @@ double mag(double a[])
     return sqrt(sqmag(a));
 }
 
-void copy(double target[], double source[])
-{
-    for (int i = 0; i < 3; ++i)
-        target[i] = source[i];
-}
-
 bool eq(double a[], double b[])
 {
     for (int i = 0; i < 3; ++i)
@@ -93,7 +101,7 @@ void set_stick(double x, double y, double z)
 {
     STICK[0] = x;
     STICK[1] = y;
-    STICK[2] = z;   
+    STICK[2] = z;
 }
 
 void set_pitch_axis(double x, double y, double z)
@@ -113,6 +121,8 @@ void set_roll_axis(double x, double y, double z)
  */
 void recalc_perps()
 {
-    copy(PITCH_PERP, cross(PITCH_AXIS, STICK));
-    copy(ROLL_PERP, cross(ROLL_AXIS, STICK));
+    double *pp = cross(PITCH_AXIS, STICK);
+    double *rp = cross(ROLL_AXIS, STICK);
+    std::copy(pp, pp + 3, PITCH_PERP);
+    std::copy(rp, rp + 3, ROLL_PERP);
 }
--- calc/angler.hpp
+++ calc/angler.hpp
@@ -6,18 +6,16 @@
 /* Assumes that everything is in R3.
  */
 
-double STICK[] = {0, 1, 0};   // direction of gravity when controller is in rest state
-double PITCH_AXIS[] = {1, 0, 0};    // direction of pitch axis when controller is in rest state
-double ROLL_AXIS[] = {0, 0, -1};    // direction of roll axis when controller is in rest state
+extern double STICK[];          // direction of gravity when controller is in rest state
+extern double PITCH_AXIS[];     // direction of pitch axis when controller is in rest state
+extern double ROLL_AXIS[];      // direction of roll axis when controller is in rest state
 
-double PITCH_PERP[] = {0, 0, 1};    // vector rotated 90 degrees counterclockwise about PITCH_AXIS from STICK direction
-double ROLL_PERP[] = {1, 0, 0};     // vector rotated 90 degrees counterclockwise about ROLL_AXIS from STICK direction
+extern double PITCH_PERP[];     // vector rotated 90 degrees counterclockwise about PITCH_AXIS from STICK direction
+extern double ROLL_PERP[];      // vector rotated 90 degrees counterclockwise about ROLL_AXIS from STICK direction
 
-double ZERO[] = {0, 0, 0};      // just the zero vector
-
-double scale_pitch = 1 / 3.0;   // responsiveness of controller [rad/s per rad]
-double scale_roll = 4 / 3.0;    // responsiveness of controller [rad/s per rad]
-double max_theta = acos(-1) / 2; // magnitude of max rotation of controller; input clipped at this value if input is higher
+extern double scale_pitch;      // responsiveness of controller [rad/s per rad]
+extern double scale_roll;       // responsiveness of controller [rad/s per rad]
+extern double max_theta;        // magnitude of max rotation of controller; input clipped at this value if input is higher
 
 double pitcher(int gx, int gy, int gz);
 double roller(int gx, int gy, int gz);
@@ -26,7 +24,6 @@ double dot(double a[], double b[]);
 double* cross(double a[], double b[]);
 double sqmag(double a[]);
 double mag(double a[]);
-void copy(double target[], double source[]);
 bool eq(double a[], double b[]);
 
 void set_stick(double x, double y, double z);
